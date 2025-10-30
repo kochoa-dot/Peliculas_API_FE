5e45715ae09fe5da6380d6804918c3d422d8ba51
Kevin Hernández <kochoa.sv@gmail.com>
2025-10-29 22:44:04 -0600

    Estilos en react

--- react-peliculas/src/App.tsx
+++ react-peliculas/src/App.tsx
@@ -1,3 +1,5 @@
+import styles from './app.module.css'//se puede importar el modulo de estilos para no interferir con otros que se puedan encontrar en la aplicacion
+
 export default function App() {
 
   const subtitulo = "Esta es una variable";
@@ -6,14 +8,20 @@ export default function App() {
 
   const imageURL = '/vite.svg';
 
+  const cuadradoRojo = {
+    backgroundColor: 'red',
+    width: '50px',
+    height: '50px',
+    marginLeft: '1rem'
+  };
+
   return(
     <>
-      <h1>Hola mundo</h1>
-      <h3>{subtitulo.toUpperCase()}</h3> 
-      {/* expresión de JSX */}
-      <h4>El doble de 3 es {duplicar(3)}</h4>
+      <h1 className="rojo">Hola mundo</h1>{/* Yo puedo asignar valores de css al codigo de react a través de el atributo className*/}
+      <div style={cuadradoRojo}></div> {/* También puedo hacerlo a traves de style con una expresión */}
+      <h3 className={styles.color}>{subtitulo.toUpperCase()}</h3> 
+      <h4>El doble de 3 es {duplicar(3)}</h4> { /* ya que aplicamos un estilo al tipo de componente se le colocarán las propiedades a cualquiera de ese tipo que es lo que impedimos con los módulos */}
       <img src={imageURL} alt="logo vite"></img>
-      {/* podemos usar expresiones de jsx para mostrar el valor de una variable al usuario, mostrar el resultado de una función, o para asignarle el valor a un atributo de un elemento html */}
     </>
   );
 }
