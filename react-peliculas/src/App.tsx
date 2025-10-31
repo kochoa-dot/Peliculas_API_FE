352e6a61874168652fe2cc0c75305800503964d3
Kevin Hernández <kochoa.sv@gmail.com>
2025-10-30 23:32:55 -0600

    Aprendiendo a manejar eventos de React

--- react-peliculas/src/App.tsx
+++ react-peliculas/src/App.tsx
@@ -1,27 +1,22 @@
-import styles from './app.module.css'//se puede importar el modulo de estilos para no interferir con otros que se puedan encontrar en la aplicacion
-
 export default function App() {
 
-  const subtitulo = "Esta es una variable";
+  const manejarClick = () => alert('click');
 
-  const duplicar = (valor: number) => valor*2;
+  const manejarKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => texto = e.currentTarget.value;
 
-  const imageURL = '/vite.svg';
-
-  const cuadradoRojo = {
-    backgroundColor: 'red',
-    width: '50px',
-    height: '50px',
-    marginLeft: '1rem'
-  };
+  let texto = '';
 
   return(
     <>
-      <h1 className="rojo">Hola mundo</h1>{/* Yo puedo asignar valores de css al codigo de react a través de el atributo className*/}
-      <div style={cuadradoRojo}></div> {/* También puedo hacerlo a traves de style con una expresión */}
-      <h3 className={styles.color}>{subtitulo.toUpperCase()}</h3> 
-      <h4>El doble de 3 es {duplicar(3)}</h4> { /* ya que aplicamos un estilo al tipo de componente se le colocarán las propiedades a cualquiera de ese tipo que es lo que impedimos con los módulos */}
-      <img src={imageURL} alt="logo vite"></img>
+    {/* Eventos en react por ejemplo cuando yo de click sobre ese elemento  */}
+      <h1 className="rojo" onClick={() => alert('Me has clickeado')}>Hola mundo</h1>
+      <button onClick={manejarClick}>Clickeame</button>
+      <div>
+        <input onKeyUp={(e) => manejarKeyUp(e)}/> {/*Cuando yo presiono una tecla y la dejo de presionar */}
+      </div>
+      <div>
+        <p>Haz escrito: {texto}</p>
+      </div>
     </>
   );
 }
