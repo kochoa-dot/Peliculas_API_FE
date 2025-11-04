c9045e26f9a0ec8eb0554f46db088a7d49f57a59
Kevin Hernández <kochoa.sv@gmail.com>
2025-11-03 23:35:18 -0600

    Usando el react hook de useState

--- react-peliculas/src/App.tsx
+++ react-peliculas/src/App.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 export default function App() {
+  //En este caso al escribir se está volviendo a ejecutar la función completa 
+  // Cuando yo cambio el estado del componente yo lo estoy volviendo a renderizar (la función completamente) En este caso el useState me permite que aunque se vuelve a ejecutar vamos a mantener el valor en diferentes renderizados
+  const [texto, setTexto] = useState("");
+  
+  let texto2 = '';
+
+  console.log("Renderizando el componente app");
+
+  console.log(texto2);
 
   const manejarClick = () => alert('click');
 
-  const manejarKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => texto = e.currentTarget.value;
-
-  let texto = '';
+  //Al hacer esto se va a volver a renderizar la función y por tanto se va a volver a escribir el valor de texto
+  const manejarKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    texto2 = e.currentTarget.value;
+    setTexto(e.currentTarget.value) 
+  };
 
   return(
     <>
