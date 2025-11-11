6d51046c4c5d3fd0a5ad04ac6394af9eff6a3a53
Kevin Hernández <kochoa.sv@gmail.com>
2025-11-10 22:50:36 -0600

    Repitiendo contenido usando map

--- react-peliculas/src/App.tsx
+++ react-peliculas/src/App.tsx
@@ -1,18 +1,10 @@
-import { useState } from "react";
-import ContenidoDinamicoIf from "./ContenidoDinamicoIf";
+import Tabla from "./Tabla";
 
 export default function App() {
 
-//Dependiendo el estado que yo he colocado en el componente padre estamos utilizando un operador ternario para mostrar contenido dinamicamente en el componente hijo
-  const[calificacion, setCalificacion] = useState<number | null>(null);
-
   return(
     <>
-      <label>Ingrese la calificación:</label>
-      <input type="number" onChange={e => setCalificacion(Number(e.target.value))}/>
-
-      {calificacion ? <ContenidoDinamicoIf calificacion={calificacion}/> : undefined}
-
+      <Tabla />
     </>
   );
 }
---
+++ react-peliculas/src/Tabla.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import type Persona from "./persona.model";
+
+export default function Tabla(){
+    const personasFuente: Persona[] = [
+        { id: 1, nombre: 'Kevin', departamento: 'Ingeniería'},
+        { id: 2, nombre: 'Adriana', departamento: 'Ingeniería'},
+        { id: 3, nombre: 'Alejandro', departamento: 'Recursos humanos'},
+        { id: 4, nombre: 'Juan', departamento: 'Contabilidad'},
+    ];
+
+    const [personas, setPersonas] = useState(personasFuente);
+
+    //La función filter me permite filtrar por lo tanto estoy diciendo que recibiré un objeto de tipo persona de parametro de esta función y estaría seleccionando la que coincide con el id del objeto que recibo
+    const removerPersona = (persona: Persona) => {
+        setPersonas(
+            personas.filter(p => p.id !== persona.id)
+        )
+    }
+    //En este caso se le asigna una llave única a cada fila del arreglo para que react sepa exactamente que fila borrar del arreglo que se recorre para generar la UI
+    //Lo correcto es que siempre que trabajemos con un elemento map le coloquemos su key al elemento padre en este caso a la fila 
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Departamento</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {personas.map(p => <tr key={p.id}>
+                    <td>{p.nombre}</td>
+                    <td>{p.departamento}</td>
+                    <td>
+                        <button onClick={() => removerPersona(p)}>Remover</button>
+                    </td>
+                </tr>)}
+            </tbody>
+        </table>
+    )
+}
